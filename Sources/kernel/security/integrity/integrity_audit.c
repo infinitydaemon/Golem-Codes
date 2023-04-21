d876eec19c23c5e5e753a25325ebc49fdb5b07ed
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-21 12:22:04 +0500

    Update integrity_audit.c

--- Sources/kernel/security/integrity/integrity_audit.c
+++ Sources/kernel/security/integrity/integrity_audit.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2008 IBM Corporation
- * Author: Mimi Zohar <zohar@us.ibm.com>
+ * Copyright (C) 2023 CWD SYSTEMS
+ * Author: CWD
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,40 +17,40 @@
 
 static int integrity_audit_info;
 
-/* ima_audit_setup - enable informational auditing messages */
+/* enable or disable informational auditing messages */
 static int __init integrity_audit_setup(char *str)
 {
 	unsigned long audit;
-
 	if (!kstrtoul(str, 0, &audit))
 		integrity_audit_info = audit ? 1 : 0;
 	return 1;
 }
 __setup("integrity_audit=", integrity_audit_setup);
 
-void integrity_audit_msg(int audit_msgno, struct inode *inode,
-			 const unsigned char *fname, const char *op,
-			 const char *cause, int result, int audit_info)
+/* write an audit message */
+void integrity_audit_msg(int audit_msgno, const char *op, const char *cause,
+			 int result, const struct inode *inode,
+			 const unsigned char *fname, int audit_info)
 {
 	struct audit_buffer *ab;
 	char name[TASK_COMM_LEN];
-
-	if (!integrity_audit_info && audit_info == 1)	/* Skip info messages */
-		return;
+	kuid_t uid = current_cred()->uid;
+	kuid_t loginuid = audit_get_loginuid(current);
+	u32 sessionid = audit_get_sessionid(current);
+	
+	if (!integrity_audit_info && audit_info == 1)
+		return; /* Skip info messages */
 
 	ab = audit_log_start(current->audit_context, GFP_KERNEL, audit_msgno);
 	audit_log_format(ab, "pid=%d uid=%u auid=%u ses=%u",
-			 task_pid_nr(current),
-			 from_kuid(&init_user_ns, current_cred()->uid),
-			 from_kuid(&init_user_ns, audit_get_loginuid(current)),
-			 audit_get_sessionid(current));
+			  task_pid_nr(current),
+			  from_kuid(&init_user_ns, uid),
+			  from_kuid(&init_user_ns, loginuid), sessionid);
 	audit_log_task_context(ab);
-	audit_log_format(ab, " op=");
-	audit_log_string(ab, op);
-	audit_log_format(ab, " cause=");
-	audit_log_string(ab, cause);
-	audit_log_format(ab, " comm=");
+
+	audit_log_format(ab, " op=%s cause=%s comm=", op, cause);
 	audit_log_untrustedstring(ab, get_task_comm(name, current));
+	
 	if (fname) {
 		audit_log_format(ab, " name=");
 		audit_log_untrustedstring(ab, fname);
