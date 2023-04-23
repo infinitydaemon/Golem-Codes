511acfdcf582867afe9d370c7e4fc6c01684d5af
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-23 12:07:58 +0500

    Update firmware-uclass.c

--- Sources/u-boot-stable-4.19-rock3/drivers/firmware/firmware-uclass.c
+++ Sources/u-boot-stable-4.19-rock3/drivers/firmware/firmware-uclass.c
@@ -1,11 +1,11 @@
 /*
- * SPDX-License-Identifier:	GPL-2.0+
+ * SPDX-License-Identifier: GPL-2.0+
  */
 
 #include <common.h>
 #include <dm.h>
 
-/* Firmware access is platform-dependent.  No generic code in uclass */
+/* Firmware access is platform-dependent. No generic code in uclass */
 UCLASS_DRIVER(firmware) = {
 	.id		= UCLASS_FIRMWARE,
 	.name		= "firmware",
@@ -14,7 +14,5 @@ UCLASS_DRIVER(firmware) = {
 U_BOOT_DRIVER(firmware) = {
 	.name	= "firmware",
 	.id	= UCLASS_FIRMWARE,
-#if !CONFIG_IS_ENABLED(OF_PLATDATA)
-	.bind = dm_scan_fdt_dev,
-#endif
+	.bind	= dm_scan_fdt_dev,
 };
